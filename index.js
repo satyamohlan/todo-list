d7d6c536dfd21fccba7e7090d469599243d2bd83
satyamohlan <satyam.ohlan1998@gmail.com>
2020-03-19 14:14:26 +0530

    bug fixes

--- index.js
+++ index.js
@@ -1,52 +1,53 @@
-const express = require("express");
+const express = require('express');
 
-const mongoose = require("mongoose");
-const task = require("./models/record");
+const mongoose = require('mongoose');
+const task = require('./models/record');
 const app = express();
-const db = require("./config/keys").MongoUri;
-require('dotenv').config();
-mongoose.connect(db, {
-    useNewUrlParser: true
+const db = require('./config/keys').MongoUri;
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   })
-  .then(() => console.log("mongo db connected"))
-  .catch(err => console.log(err))
+  .then(() => console.log('mongo db connected'))
+  .catch(err => console.log(err));
 //app.use(expressLayouts);
-app.set("view engine", 'ejs');
-
-app.use(express.urlencoded({extended: false}));
-const port=process.env.PORT||3000;
+app.set('view engine', 'ejs');
+app.use('/css', express.static('views/css'));
+app.use(express.urlencoded({ extended: false }));
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log("running at "+port);
+  console.log('running at ' + port);
 });
-app.use("/css", express.static("views/css"));
-app.get("/todo", (req, res) => {
+app.get('/', (req, res) => {
+  task.find({}, (err, data) => {
+    if (err) {
+      throw err;
+    }
 
-  task.find({},(err,data)=>{
-    if (err){throw err;}
-   
-  res.render("todo",{data});
+    res.render('todo', { data });
   });
-  
-})
+});
 
 app.post('/todo', (req, res) => {
-    var {str}=req.body;
+  var { str } = req.body;
   console.log(str);
-var ntask=new task({task:str});
-ntask.save((err,json)=>{
-if (err){throw err;}
-else{console.log("sucesss");}
+  var ntask = new task({ task: str });
+  ntask.save((err, json) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log('sucesss');
+    }
+  });
+  console.log(ntask);
+  res.redirect('/');
 });
-console.log(ntask);
-res.redirect("/todo");
-
-})
 app.delete('/todo', (req, res) => {
-
-task.deleteOne({task:req.query.item},(err,data)=>{
-  if (err) throw err;
-  else{
-   res.json(data);
-  }
-})
-  })
+  task.deleteOne({ task: req.query.item }, (err, data) => {
+    if (err) throw err;
+    else {
+      res.json(data);
+    }
+  });
+});
